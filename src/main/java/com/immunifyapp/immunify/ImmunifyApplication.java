85cb7073d711dd972c22cc87860a2d753448eda0
REZ-OAN <ahmedabir.rez@gmail.com>
2023-11-17 09:35:50 +0600

    get started

---
+++ src/main/java/com/immunifyapp/immunify/ImmunifyApplication.java
@@ -0,0 +1,13 @@
+package com.immunifyapp.immunify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmunifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmunifyApplication.class, args);
+	}
+
+}
